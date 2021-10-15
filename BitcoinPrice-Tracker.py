962c76a5d3e28a5ffe47e0979f4a52799fc42b8c
Nitish <55824689+nitishchaudhary@users.noreply.github.com>
2021-10-15 14:27:59 +0530

    Email notification added

--- BitcoinPrice-Tracker.py
+++ BitcoinPrice-Tracker.py
@@ -2,6 +2,8 @@ from plyer import notification
 import time
 import requests
 import json
+import smtplib
+from email.message import EmailMessage
 alert_price = int(input("Enter the price for alert! :"))
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 check = True
@@ -23,6 +25,25 @@ while(True):
 			title = "Price Alert",
 			message = f"Price: {Bitcoin_value}",	
 			timeout = 5)
+		email_message = ""
+		email = EmailMessage()
+		email["from"] = "Bitcoin-Notifier"
+		email["to"] = "006nschaudhary006@gmail.com"
+		email["subject"] = "!Bitcoin Price drop!"
+		email.set_content(email_message)
+
+		#setting up username and password for gmail account
+		email_id = "ncnitish6250@gmail.com"
+		password = " "
+
+		#initializing smtp server
+		with smtplib.SMTP(host = "smtp.gmail.com" , port = 587) as smtp:
+			smtp.ehlo()
+			#connecting securely to the server
+			smtp.starttls()
+			smtp.login(email_id , password)
+			smtp.send_message(email)
+
 		check = False
 	else:
 		check = True
